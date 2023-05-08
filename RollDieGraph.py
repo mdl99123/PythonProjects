a19365ecddf24618ffeead22eaec0cf19d08c7c6
mdl123 <mdlama99@gmail.com>
2023-05-08 13:30:28 -0500

    Update RollDieGraph.py
    Added user input to the program.

--- RollDieGraph.py
+++ RollDieGraph.py
@@ -6,17 +6,27 @@ import random
 import seaborn as sns
 
 # %% 
-rolls = [random.randrange(1,7) for i in range(600)]
-values, frequencies = np.unique(rolls, return_counts=True)
-title = f'Rolling a Six-Sided Die {len(rolls):,} Times'
-sns.set_style("whitegrid")
-axes = sns.barplot(x=values,y=frequencies)
-axes.set_title(title)
-axes.set(xlabel='Die Value', ylabel='Frequency')
-axes.set_ylim(top=max(frequencies)*1.10)
-for bar, frequency in zip(axes.patches, frequencies):
-    text_x = bar.get_x() + bar.get_width() / 2.0
-    text_y = bar.get_height()
-    text= f'{frequency:,}\n{frequency / len(rolls):.3%}'
-    axes.text(text_x, text_y, text, fontsize=11, ha='center', va='bottom')
-plt.show()
+option='-1'
+while option!='2':
+    print("Enter the amount of die rolls: ")
+    amount=input()
+    try:
+        rolls = [random.randrange(1,7) for i in range(int(amount))]
+        values, frequencies = np.unique(rolls, return_counts=True)
+        title = f'Rolling a Six-Sided Die {len(rolls):,} Times'
+        sns.set_style("whitegrid")
+        axes = sns.barplot(x=values,y=frequencies)
+        axes.set_title(title)
+        axes.set(xlabel='Die Value', ylabel='Frequency')
+        axes.set_ylim(top=max(frequencies)*1.10)
+        for bar, frequency in zip(axes.patches, frequencies):
+            text_x = bar.get_x() + bar.get_width() / 2.0
+            text_y = bar.get_height()
+            text= f'{frequency:,}\n{frequency / len(rolls):.3%}'
+            axes.text(text_x, text_y, text, fontsize=11, ha='center', va='bottom')
+        plt.show()
+        print("1.- Run another simulation")
+        print("2.- Exit program")
+        option=input()
+    except BaseException:
+        print("Error most likely due to input error")
